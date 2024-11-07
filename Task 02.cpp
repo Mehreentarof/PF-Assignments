0372c23dcef172f5783099dfa034e939236d8d76
Mrhreen Tarof <muhammadtarof76@gmail.com>
2024-11-07 21:30:21 +0500

    Add files via upload

---
+++ Task 02.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    double Product_A_Price=100;
+    double Product_B_Price=80;
+    double Product_C_Price=60;
+    int QuantityA, QuantityB, QuantityC;
+    cout<<"Enter the quantity of product A:";
+    cin>>QuantityA;
+    cout<<"Enter the quantity of product B:";
+    cin>>QuantityB;
+    cout<<"Enter the quantity of product C:";
+    cin>>QuantityC;
+    double Total=(Product_A_Price*QuantityA)+(Product_B_Price*QuantityB)+(Product_C_Price*QuantityC);
+    cout<<"Total:$"<<Total<<endl;
+    double Totaldiscount=(Total>200)? Total*0.9:Total;
+    cout<<"Total after discount:$"<<Totaldiscount<<endl;
+    return 0;
+}
